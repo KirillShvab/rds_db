65a3944359da222ac0c06b8edf1ddf79f98e534b
Kirill Schwab <k.d.shvab@tmn4.etagi.com>
2025-10-30 17:40:29 +0500

    update select deals+realtor

--- deals/deals/deals.sql
+++ deals/deals/deals.sql
@@ -10,21 +10,29 @@ WITH dates_cte AS (
             ELSE DATE_TRUNC('month', CURRENT_DATE)::timestamp
         END AS actual_date
 ),
+/*
+    Формирует сделки риэлторов по дате среза с булевыми флагами по типу объекта 
+    (гараж, загородная, вторичная квартира, коммерческая, новостройка) 
+    для сотрудников отдела продаж из выбранных подразделений
+*/
 deals AS (
     SELECT
         d.date_deal_findep,
         d.city_name,
-        d.rieltor_name, 
         d.rieltor_id,
-        e.date_employment 
-    FROM reports."278_6_deal" AS d
+		CASE WHEN object_class = 'Гараж' THEN TRUE ELSE FALSE END AS garage,
+		CASE WHEN object_class = 'Загородная' THEN TRUE ELSE FALSE END AS country_house,
+		CASE WHEN object_class IN ('Вторичная', 'Квартира') THEN TRUE ELSE FALSE END AS secondary_apartment,
+		CASE WHEN object_class = 'Коммерческая' THEN TRUE ELSE FALSE END AS commercial,
+		CASE WHEN object_class = 'Новостройка' THEN TRUE ELSE FALSE END AS new_building
+	FROM reports."278_6_deal" AS d
     JOIN dates_cte AS dt ON TRUE
     JOIN hr_new.employees AS e
         ON d.rieltor_id = e.employee_id
         AND e.period = dt.actual_date
     JOIN hr_new.cities AS c
     ON e.city_name = c.city_name 
-    WHERE d.date_deal_findep >= dt.actual_date
+    WHERE  d.date_deal_findep >= dt.actual_date
       AND d.date_deal_findep < (dt.actual_date + INTERVAL '1 month')
       AND d.msd_rieltor_result = 'Риэлтор'
       AND LOWER(d.employee_group_0) = 'нет'
@@ -44,17 +52,20 @@ deals AS (
       )
 ),
 /*
-    Запрос формирует выборку межрегиональных сделок за актуальный месяц 
-    (или за предыдущий, если сегодня 1–3 число месяца), 
-    с последующим подсчётом количества таких сделок.
+    Формирует межрегиональные сделки риэлторов по дате закрытия месяца с булевыми флагами по типу объекта 
+    (гараж, загородная, вторичная квартира, коммерческая, новостройка)
+    для сотрудников отдела продаж с фильтром по статусу сделки, типу комиссии и подразделению.
 */
 interregional_deals AS (
     SELECT 
         d.date_close_month,
         d.city_name,
-        d.rieltor_name, 
         d.rieltor_id,
-        e.date_employment 
+        CASE WHEN object_class = 'Гараж' THEN TRUE ELSE FALSE END AS garage,
+		CASE WHEN object_class = 'Загородная' THEN TRUE ELSE FALSE END AS country_house,
+		CASE WHEN object_class IN ('Вторичная', 'Квартира') THEN TRUE ELSE FALSE END AS secondary_apartment,
+		CASE WHEN object_class = 'Коммерческая' THEN TRUE ELSE FALSE END AS commercial,
+		CASE WHEN object_class = 'Новостройка' THEN TRUE ELSE FALSE END AS new_building
     FROM reports."532_187_interregional_commissions" AS d
     JOIN dates_cte AS dt ON TRUE
     JOIN hr_new.employees AS e 
@@ -62,8 +73,8 @@ interregional_deals AS (
         AND e.period = dt.actual_date
     JOIN hr_new.cities AS c
     ON e.city_name = c.city_name 
-    WHERE d.date_close_month >= dt.actual_date
-      AND d.date_close_month < (dt.actual_date + INTERVAL '1 month')
+    WHERE d.date_close_month >= dt.actual_date 
+    AND d.date_close_month < (dt.actual_date + INTERVAL '1 month')
     AND e.employee_type = 'Специалист отдела продаж'
     AND d.msd_rieltor_result = 'Риэлтор'
     AND LOWER(d.employee_group_0) = 'Нет'
@@ -111,21 +122,30 @@ interregional_deals AS (
         )
     )
 ),
-general_deals as 
-(
-    select  date_deal_findep AS deal_date,
-            city_name,
-            rieltor_name, 
-            rieltor_id,
-            date_employment 
-    from deals
+/*
+Объединяет все сделки риэлторов из CTE deals и interregional_deals
+в единый набор с булевыми флагами по типу объекта и датой сделки.
+ */
+general_deals as (
+    select  d.date_deal_findep AS deal_date,
+            d.city_name,
+            d.rieltor_id,
+            d.garage,                          		
+		    d.country_house,            		
+		    d.secondary_apartment,       
+		    d.commercial,                  		
+		    d.new_building AS new_building              		
+    from deals AS d
     UNION ALL
-    select  date_close_month AS deal_date,
-            city_name,
-            rieltor_name, 
-            rieltor_id,
-            date_employment 
-    from interregional_deals
+    select  di.date_close_month AS deal_date,
+            di.city_name,
+            di.rieltor_id,
+            di.garage,                          		
+		    di.country_house,            		
+		    di.secondary_apartment,       
+		    di.commercial,                  		
+		    di.new_building AS new_building              		
+    from interregional_deals AS di
 )
 select count(*)
 from general_deals
--- rieltors/rieltors.sql
+++ rieltors/rieltors.sql
@@ -7,35 +7,38 @@ WITH dates_cte AS (
         END AS actual_date
 ),
 /*rieltors формирует список активных риэлторов на текущий отчетный месяц*/
-rieltors AS
-(
+rieltors AS (
     SELECT 
-        dt.actual_date as date_slice,
+        dt.actual_date AS date_slice,
         e.city_name,
         e.hsd_name,
         e.msd_name,
-        e.employee_name as rieltor_name,
-        e.employee_id as rieltor_id,
-        e.date_employment
+        e.employee_name AS rieltor_name,
+        e.employee_id AS rieltor_id,
+        e.date_employment,
+        e.experience_month AS experience_metric,
+        ((EXTRACT(YEAR FROM dt.actual_date) - EXTRACT(YEAR FROM e.date_employment)) * 12
+              + (EXTRACT(MONTH FROM dt.actual_date) - EXTRACT(MONTH FROM e.date_employment))
+              + CASE WHEN EXTRACT(DAY FROM e.date_employment) <= 10 THEN 1 ELSE 0 END
+            )::int AS old_experience_month
     FROM hr_new.employees AS e
     JOIN dates_cte AS dt ON TRUE
     WHERE
-    e.period = dt.actual_date
-    AND e.employee_work_status IN ('В ежегодном отпуске', 'В работе', 'Приостановить прием заявок') 
-    AND e.employee_status = 'Активен' -- тут возможны однозначно проблемы
-    AND e.employee_type = 'Специалист отдела продаж'
-    AND e.employee_work_type = 'Риэлтор' 
-    AND e.employee_group_0 = FALSE -- тут возможны проблемы
-    AND e.employee_status_priority = True
-    AND e.department_name IN (  'Департамент межрегиональных сделок', 
-                                'Департамент новостроек',
-                                'Департамент коммерческой недвижимости',
-                                'Департамент продаж', 
-                                'Отдел загородной недвижимости',
-                                'Отдел коммерческой недвижимости', 
-                                'Отдел новостроек',
-                                'Отдел продаж')
-)
-select *
-from rieltors
-LIMIT 10;
+        e.period = dt.actual_date
+        AND e.employee_work_status IN ('В ежегодном отпуске', 'В работе', 'Приостановить прием заявок')
+        --AND e.employee_status = 'Активен'  -- проблемная зона, не всегда актуальные статусы
+        AND e.employee_type = 'Специалист отдела продаж'
+        AND e.employee_work_type = 'Риэлтор'
+        AND e.employee_group_0 = FALSE
+        AND e.employee_status_priority = TRUE
+        AND e.department_name IN (
+            'Департамент межрегиональных сделок', 
+            'Департамент новостроек',
+            'Департамент коммерческой недвижимости',
+            'Департамент продаж', 
+            'Отдел загородной недвижимости',
+            'Отдел коммерческой недвижимости', 
+            'Отдел новостроек',
+            'Отдел продаж'
+        )
+),
