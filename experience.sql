144d01abf7e3fde239e4691bc41809978aa18b80
Kirill Schwab <k.d.shvab@tmn4.etagi.com>
2025-11-01 17:02:22 +0500

    final validated deals

--- experience.sql
+++ experience.sql
@@ -14,9 +14,9 @@ WITH dates_cte AS (
     - Добавляются метрики опыта:
         * experience_ten_days — общий показатель опыта из таблицы сотрудников.
         * old_experience_ten_days — вычисленный опыт в месяцах на дату среза, с бонусом +1, если день трудоустройства ≤ 10.
- * */
+ */
 filtred_rieltors AS (
- SELECT 
+    SELECT 
         dt.actual_date AS date_slice,
         e.city_name,
         e.hsd_name,
@@ -33,22 +33,19 @@ filtred_rieltors AS (
     JOIN dates_cte AS dt ON TRUE
     WHERE
         e.period = dt.actual_date
-        AND e.employee_work_status IN ('В ежегодном отпуске', 'В работе', 'Приостановить прием заявок')
-        --AND e.employee_status = 'Активен'  -- проблемная зона, не всегда актуальные статусы
+        -- AND e.employee_work_status IN ('В ежегодном отпуске', 'В работе', 'Приостановить прием заявок') -- сделка есть а человек на больничном
+        -- AND e.employee_status = 'Активен'  -- проблемная зона, не всегда актуальные статусы
         AND e.employee_type = 'Специалист отдела продаж'
         AND e.employee_work_type = 'Риэлтор'
-        AND e.employee_group_0 = FALSE
         AND e.employee_status_priority = TRUE
-        AND e.department_name IN (
-            'Департамент межрегиональных сделок', 
-            'Департамент новостроек',
-            'Департамент коммерческой недвижимости',
-            'Департамент продаж', 
-            'Отдел загородной недвижимости',
-            'Отдел коммерческой недвижимости', 
-            'Отдел новостроек',
-            'Отдел продаж'
-        )
+        AND e.department_name IN ( 'Отдел новостроек',
+                                   'Отдел продаж',
+                                   'Департамент межрегиональных сделок',
+                                   'Департамент новостроек',
+                                   'Департамент продаж',
+                                   'Отдел загородной недвижимости',
+                                   'Отдел коммерческой недвижимости',
+                                   'Отдел межрегиональных и международных сделок')
 ),
 rieltors AS (
 	SELECT  
@@ -83,36 +80,19 @@ deals AS (
         d.date_deal_findep,
         d.city_name,
         d.rieltor_id,
-		CASE WHEN object_class = 'Гараж' THEN TRUE ELSE FALSE END AS garage,
+        -- мы явно указываем, требуется уточнение по перечню object_class - нужно что-то еще?
+		-- CASE WHEN object_class IN ('Гараж', 'Гаражи') THEN TRUE ELSE FALSE END AS garage, -- это нам не интересно
 		CASE WHEN object_class = 'Загородная' THEN TRUE ELSE FALSE END AS country_house,
 		CASE WHEN object_class IN ('Вторичная', 'Квартира') THEN TRUE ELSE FALSE END AS secondary_apartment,
+		CASE WHEN object_class IN ('Новостройка','Новостройки') THEN TRUE ELSE FALSE END AS new_building,
 		CASE WHEN object_class = 'Коммерческая' THEN TRUE ELSE FALSE END AS commercial,
-		CASE WHEN object_class = 'Новостройка' THEN TRUE ELSE FALSE END AS new_building
+		CASE WHEN object_class = 'Прочие поступления' THEN TRUE ELSE FALSE END AS other_income
 	FROM reports."278_6_deal" AS d
     JOIN dates_cte AS dt ON TRUE
-    JOIN hr_new.employees AS e
-        ON d.rieltor_id = e.employee_id
-        AND e.period = dt.actual_date
-    JOIN hr_new.cities AS c
-    ON e.city_name = c.city_name 
-    WHERE  d.date_deal_findep >= dt.actual_date
+    WHERE d.date_deal_findep >= dt.actual_date
       AND d.date_deal_findep < (dt.actual_date + INTERVAL '1 month')
-      AND d.msd_rieltor_result = 'Риэлтор'
       AND LOWER(d.employee_group_0) = 'нет'
-      AND e.employee_status_priority = TRUE
       AND LOWER(d.deal_type_sell_buyer) IN ('покупка', 'продажа') 
-      AND e.employee_type = 'Специалист отдела продаж'
-      AND e.department_name IN (
-            'Департамент межрегиональных сделок', 
-            'Департамент новостроек',
-            'Департамент продаж', 
-            'Отдел загородной недвижимости',
-            'Отдел коммерческой недвижимости', 
-            'Отдел новостроек',
-            'Отдел продаж',
-		    'Департамент коммерческой недвижимости',
-		    'Отдел межрегиональных и международных сделок'
-      )
 ),
 /*
     Формирует межрегиональные сделки риэлторов по дате закрытия месяца с булевыми флагами по типу объекта 
@@ -124,39 +104,24 @@ interregional_deals AS (
         d.date_close_month,
         d.city_name,
         d.rieltor_id,
-        CASE WHEN object_class = 'Гараж' THEN TRUE ELSE FALSE END AS garage,
+        -- мы явно указываем, требуется уточнение по перечню object_class - нужно что-то еще?
+        -- CASE WHEN object_class = 'Гараж' THEN TRUE ELSE FALSE END AS garage, -- это нам не интересно
 		CASE WHEN object_class = 'Загородная' THEN TRUE ELSE FALSE END AS country_house,
 		CASE WHEN object_class IN ('Вторичная', 'Квартира') THEN TRUE ELSE FALSE END AS secondary_apartment,
 		CASE WHEN object_class = 'Коммерческая' THEN TRUE ELSE FALSE END AS commercial,
-		CASE WHEN object_class = 'Новостройка' THEN TRUE ELSE FALSE END AS new_building
+		CASE WHEN object_class IN ('Новостройка','Новостройки') THEN TRUE ELSE FALSE END AS new_building,
+		CASE WHEN object_class = 'Прочие поступления' THEN TRUE ELSE FALSE END AS other_income
     FROM reports."532_187_interregional_commissions" AS d
     JOIN dates_cte AS dt ON TRUE
-    JOIN hr_new.employees AS e 
-        ON d.rieltor_id = e.employee_id
-        AND e.period = dt.actual_date
-    JOIN hr_new.cities AS c
-    ON e.city_name = c.city_name 
     WHERE d.date_close_month >= dt.actual_date 
     AND d.date_close_month < (dt.actual_date + INTERVAL '1 month')
-    AND e.employee_type = 'Специалист отдела продаж'
-    AND d.msd_rieltor_result = 'Риэлтор'
-    AND LOWER(d.employee_group_0) = 'Нет'
-    AND e.employee_status_priority = True
+    AND LOWER(d.employee_group_0) = 'нет'
     AND
     (
         (   d.kosmos = 0
             AND d.deal_status = 'Закрыта (успех)'
-            AND d.city_name IS NOT NULL
             AND d.deal_sub_type NOT IN ('Общий')
-            AND e.department_name IN (  'Департамент межрегиональных сделок',
-                                        'Департамент новостроек',
-                                        'Департамент продаж',
-                                        'Отдел загородной недвижимости',
-                                        'Отдел коммерческой недвижимости',
-                                        'Отдел межрегиональных и международных сделок',
-                                        'Отдел новостроек',
-                                        'Отдел продаж')
-            AND d.mortgage_ticket_type IN (  'Новостройки',
+            AND d.mortgage_ticket_type IN ( 'Новостройки',
                                             'Покупка вторичная',
                                             'Покупка гаражи',
                                             'Покупка загородной',
@@ -172,15 +137,7 @@ interregional_deals AS (
         OR
         (   d.kosmos = 1
             AND d.deal_status = 'Закрыта'
-            AND e.department_name IN (  'Департамент коммерческой недвижимости', 
-                                        'Департамент межрегиональных сделок', 
-                                        'Департамент новостроек', 
-                                        'Департамент продаж', 
-                                        'Отдел коммерческой недвижимости', 
-                                        'Отдел межрегиональных и международных сделок', 
-                                        'Отдел продаж')           
-            AND d.mortgage_ticket_type IN (  'Покупка',
-                                            'Продажа')
+            AND LOWER(d.mortgage_ticket_type) IN ('покупка', 'продажа')
             AND d.revenue_type IN ('Сделка')
         )
     )
@@ -197,7 +154,8 @@ general_deals as (
 		    d.country_house,            		
 		    d.secondary_apartment,       
 		    d.commercial,                  		
-		    d.new_building AS new_building              		
+		    d.new_building AS new_building,
+		    d.other_income
     from deals AS d
     UNION ALL
     select  di.date_close_month AS deal_date,
@@ -207,26 +165,30 @@ general_deals as (
 		    di.country_house,            		
 		    di.secondary_apartment,       
 		    di.commercial,                  		
-		    di.new_building AS new_building              		
+		    di.new_building AS new_building,
+		    di.other_income
     from interregional_deals AS di
 )
 SELECT
+	d.deal_date,
     d.city_name,
     r.hsd_name,
     r.msd_name,
     r.rieltor_name,
+    r.rieltor_id,
+    r.date_employment,
     r.experience_ten_days,
     r.old_experience_ten_days,
     r.experience,
     r.old_experience,
-    d.garage,                          	-- гараж  
+    --d.garage,                          	-- гараж  
     d.country_house,            		-- загородная недвижимость  
     d.secondary_apartment,       		-- квартира + вторичная
     d.commercial,                  		-- коммерческая недвижимость  
-    d.new_building              		-- новостройка  
+    d.new_building,						-- новостройка  
+    d.other_income 
 FROM general_deals AS d
-JOIN rieltors AS r
-    ON r.rieltor_id = d.rieltor_id
+ JOIN rieltors AS r 
+        ON d.rieltor_id = r.rieltor_id 
 WHERE 
- (d.garage OR d.country_house OR d.secondary_apartment OR d.commercial OR d.new_building)
-LIMIT 1000;
+ ( d.country_house OR d.secondary_apartment OR d.commercial OR d.new_building OR  d.other_income )
