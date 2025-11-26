5376f71cc623d658e4ce0d899296a7a67d453dc8
Kirill Schwab <kirill_shvab@mail.ru>
2025-11-26 15:04:03 +0500

    remove trash

--- fuction/load_rds_deals.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE rmd_new.load_rieltors_experience()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    INSERT INTO rmd_new.rieltors_experience_slice
-    SELECT *
-    FROM rmd_new.get_rieltors_experience_slice();
-END;
-$$;
--- fuction/rds_deals.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE rmd_new.rds_deals()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    CALL rmd_new.create_or_truncate_rieltors_experience_slice();
-    CALL rmd_new.load_rieltors_experience();
-END;
-$$;
